f12b1f6c6df191c65a8c38df151c023f6222271c
jrfarah <jrfarah1999@gmail.com>
2016-04-05 21:24:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 from Tkinter import *
 from tkFileDialog import askdirectory as selectFolder
+import os
 
 main = Tk()
 
-folder_to_back_up = "kjsnfkjsfkwjhfvbw"
-backup_location = "cmdnvsvlkslv"
+folder_to_back_up = "Select the folder you want to backup here:=======>"
+backup_location = "Select backup location here ==============>"
 # functions
 def select_folder_location():
 	'''what folder do you want to backup?'''
-	print "select_folder_location"
+	global folder_to_back_up_label
+	folder_to_back_up = selectFolder()
+	folder_to_back_up_label.destroy()
+	folder_to_back_up_label = Label(main, text = folder_to_back_up)
+	folder_to_back_up_label.grid(row = 1, column = 0)
+	print type(folder_to_back_up)	
 
 def select_backup_location():
+	global backup_location_label
+	backup_location_label.destroy()
 	'''where do you want to back it up'''
-	print "select_backup_location"
+	backup_location = selectFolder()
+	backup_location_label = Label(main, text = backup_location)
+	backup_location_label.grid(row=2, column=0)
+	print type(backup_location)
+
+def backup():
+	global backup_location, folder_to_back_up
+	command = "cp -r {0} {1}".format(folder_to_back_up, backup_location)
+	os.system(command)
 
 # Buttons and labels
-#Labels if file locations are already selected
-folder_to_back_up_label = Label(main, text = folder_to_back_up) 
+folder_to_back_up_label = Label(main, text = folder_to_back_up)
 folder_to_back_up_label.grid(row = 1, column = 0)
 backup_location_label = Label(main, text = backup_location)
 backup_location_label.grid(row = 2,column = 0)
 select_folder_location_button = Button(main, 
-						text = "What folder do you want to back up?",
+						text = "Folder to back up",
 						command = select_folder_location)
-select_folder_location_button.grid(row = 2, column = 1)
+select_folder_location_button.grid(row = 1, column = 1)
+select_backup_location_button = Button(main, text = "Backup location", command = select_backup_location)
+select_backup_location_button.grid(row = 2, column = 1)
+backup_button = Button(main, text = 'BACKUP BACKUP BACKUP BACKUP BACKUP BACKUP BACKUP', command = backup)
+backup_button.grid(row = 3, column = 0)
 
-
-main.mainloop()
+main.mainloop() 
